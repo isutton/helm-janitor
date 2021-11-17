6cbc9c6800b7573148c6b1b18f5ba7494985651d
Igor Sutton <isuttonl@redhat.com>
2021-11-17 14:32:49 +0100

    chore: remove boilerplate documentation

--- cmd/root.go
+++ cmd/root.go
@@ -12,17 +12,8 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "helm_janitor",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Use:   "helm-janitor",
+	Short: "A Helm plugin that remove failed releases revisions from the cluster",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
